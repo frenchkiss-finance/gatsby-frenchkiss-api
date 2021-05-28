04997bbdd78010a26d89ad1ed9353afb52e24a48
Frenchkiss Finance <hello@frenchkiss.finance>
2021-05-28 21:09:33 +0100

    chore: Removed masterchef address

--- api/pending.ts
+++ api/pending.ts
@@ -9,7 +9,7 @@ const getBalanceNumber = (balance: any, decimals = 18) => {
 };
 
 const pending = async (pid: number, address: string) => {
-  const chef = getContract(chefABI, "0x73feaa1eE314F8c655E354234017bE2193C9E24E");
+  const chef = getContract(chefABI, "");
   const pending = await chef.methods.pendingKiss(pid, address).call();
   const poolInfo = await chef.methods.poolInfo(pid).call();
   return {
@@ -19,7 +19,7 @@ const pending = async (pid: number, address: string) => {
 };
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
-  const { address = "0x0F9399FC81DaC77908A2Dde54Bb87Ee2D17a3373", pid = "1" } = req.query;
+  const { address = "0x0b5E8214562dd59e204717CEe782904EC4B12B6D", pid = "1" } = req.query;
   if (Array.isArray(pid)) {
     res.status(400).send({ error: "Parameter Incorrect" });
   } else {
